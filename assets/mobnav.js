d8bea13f1e5ef97aeb3094b993a3227d8bf7814b
Luke <lukescott12345@gmail.com>
2021-05-17 22:05:19 +0100

    New layout and stramedlined content to new branding

---
+++ assets/mobnav.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "280px";
+    document.getElementById("mySidenav-back").style.width = "100%";
+
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav-back").style.width = "0";
+
+}
